332cc05a1bc5f0e5bf5134fb01a42132ed4fe993
lifeisgoodlg <guswls3804@naver.com>
2026-04-28 09:50:53 +0900

    커밋

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '43.200.225.126',
+    'localhost'
+]
 
 
 # Application definition
